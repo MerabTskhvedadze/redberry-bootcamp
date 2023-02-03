29ea433344b5a2d57b56c0538a3cb3cb451185a0
MerabTskhvedadze <merabi.tskhvedadze@gmail.com>
2023-02-03 23:42:18 +0400

    added loading spinner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,18 +7,17 @@ import logo from '../../assets/images/redberry.png';
 import landingImg from '../../assets/images/shutterstock_2189773743.png';
 
 const Home = () => {
-  // return (
-  //   <>
-  //     <img className='landingImg' src={landingImg} alt='landing img' />
-  //     <main className='main'>
-  //       <Header>
-  //         <img className='main__logo' src={logo} alt='redberry' />
-  //       </Header>
-  //       <Button className='main__btn'>ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ</Button>
-  //     </main>
-  //   </>
-  // );
-  return <LoadingSpinner />;
+  return (
+    <>
+      <img className='landingImg' src={landingImg} alt='landing img' />
+      <main className='main'>
+        <Header>
+          <img className='main__logo' src={logo} alt='redberry' />
+        </Header>
+        <Button className='main__btn'>ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ</Button>
+      </main>
+    </>
+  );
 };
 
 export default Home;
